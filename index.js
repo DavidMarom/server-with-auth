3764af7c21ac9574f7124c64051d49109bd2753a
David Marom <davidmarom@hotmail.com>
2025-09-26 12:17:34 +0300

    seperated to routes file

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const cors = require('cors')
 const express = require('express')
+const routes = require('./routes/routes');
+
 const app = express()
+
 
 app.use(cors({
     origin: 'http://localhost:3001',      // must be the exact origin (no '*')
@@ -10,18 +13,7 @@ app.use(cors({
 }));
 
 app.use(express.json());
-const port = 3000
 
-app.get("/login", (req, res) => {
-    res.cookie("token", "12345", {
-        httpOnly: true, // prevents client-side JS from accessing the cookie
-        secure: false, // set to true if using HTTPS
-        sameSite: "lax", // prevents CSRF
-        path: "/", // cookie is accessible on the entire site
-        maxAge: 1000 * 10 * 60 // cookie expires in 10 minutes
-    });
-    res.json("Cookie has been set");
-});
+app.use('/', routes);
 
-
-app.listen(port, () => { console.log(`Listening on port ${port}`) })
+app.listen(3000, () => { console.log(`=== SERVER STARTED ===`) })
